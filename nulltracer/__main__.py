a387cd55592a36e3008e8465216b1ce7b24b6068
Juan Altmayer Pizzorno <juan@altmayer.com>
2022-08-19 12:22:22 -0400

    - fixed packaging;
    - added argv checking;

--- nulltracer/__main__.py
+++ nulltracer/__main__.py
@@ -7,14 +7,14 @@ def start_trace():
 
     atexit.register(lambda: print("tracer called: ", nulltracer.get_count()))
 
-
-if sys.argv[1] == '-m':
-    import runpy
-    sys.argv = sys.argv[2:]
-    start_trace()
-    runpy.run_module(sys.argv[0], run_name='__main__', alter_sys=True)
-else:
-    code = compile(Path(sys.argv[1]).read_text(), sys.argv[1], "exec")
-    sys.argv = sys.argv[1:]
-    start_trace()
-    exec(code)
+if len(sys.argv) > 1:
+    if sys.argv[1] == '-m':
+        import runpy
+        sys.argv = sys.argv[2:]
+        start_trace()
+        runpy.run_module(sys.argv[0], run_name='__main__', alter_sys=True)
+    else:
+        code = compile(Path(sys.argv[1]).read_text(), sys.argv[1], "exec")
+        sys.argv = sys.argv[1:]
+        start_trace()
+        exec(code)
--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from distutils.core import setup, Extension
 setup(name = 'nulltracer',
       version = '1.0',
       description = 'Test null tracer',
+      packages=['nulltracer'],
       ext_modules = [
           Extension('nulltracer.nulltracer', sources = ['nulltracer.cxx'],
                     extra_compile_args=['-std=c++17'])
